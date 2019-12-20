fffef65d6348ff3232af0b0d6f29cabae5f48e17
Millette-G <millette.gm@gmail.com>
2019-12-20 13:29:06 -0600

    In the dinosaur section, everything works, except the roar function. Trouble shooting now.

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -8,51 +8,53 @@
 
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
 
-const tyrannosaurus = (name, diet, weight, length, period => {
-  this.name = 'tyrannosaurus',
-  this.diet = 'carnivorous',
-  this.weight = '7000kg',
-  this.length = '12m',
-  this.period = 'Late Cretaceous'
-});
+const tyrannosaurus = {
+    name: 'tyrannosaurus',
+    diet: 'carnivorous',
+    weight: '7000kg',
+    length: '12m',
+    period: 'Late Cretaceous',
+    roar: function() {
+      return 'RAWERSRARARWERSARARARRRR!';
+    },
+}
 
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
 
-const stegosaurus = (name, diet, weight, length, period => {
-  this.name = 'stegosaurus',
-  this.diet = 'herbivorous',
-  this.weight = '2000kg',
-  this.length = '9m',
-  this.period = 'Late Jurassic'
-});
+const stegosaurus = {
+  name: 'stegosaurus',
+  diet: 'herbivorous',
+  weight: '2000kg',
+  length: '9m',
+  period: 'Late Jurassic'
+}
 
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
 
-const velociraptor = (name, diet, weight, length, period => {
-  this.name = 'velociraptor',
-  this.diet = 'carnivorous',
-  this.weight = '15kg',
-  this.length = '1.8m',
-  this.period = 'Late Cretaceous'
-});
+const velociraptor = {
+  name: 'velociraptor',
+  diet: 'carnivorous',
+  weight: '15kg',
+  length: '1.8m',
+  period: 'Late Cretaceous'
+};
 
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(tyrannosaurus.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(velociraptor.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(stegosaurus.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
-
+console.log(tyrannosaurus.period);
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+console.log(tyrannosaurus.roar());
 
 
 // ==== Arrays ====
